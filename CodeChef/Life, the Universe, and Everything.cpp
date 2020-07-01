cc952aab207bb8aad412928bb52d8df20889ca8a
Sabbir Ahmed <sabbir.hstu.19@gmail.com>
2020-07-01 21:21:28 +0600

    C++

---
+++ CodeChef/Life, the Universe, and Everything.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    while(1)
+    {
+        cin >> x;
+        if(x!=42)
+            cout << x << endl;
+        else
+            break;
+    }
+    return 0;
+
+}
