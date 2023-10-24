da2261f1d6535f063ed0aec8c6f3038cb8d2d62e
sabbir-veer <sabbir.hstu.19@gmail.com>
2023-10-24 23:54:45 +0600

    Javascript 30 days challenge

---
+++ Leetcode/2666. Allow One Function Call.js
@@ -0,0 +1,17 @@
+var once = function (fn) {
+  let counter = 0;
+  return function (...args) {
+    if (!counter) {
+      counter++;
+      return fn(...args);
+    } else return;
+  };
+};
+
+/**
+ * let fn = (a,b,c) => (a + b + c)
+ * let onceFn = once(fn)
+ *
+ * onceFn(1,2,3); // 6
+ * onceFn(2,3,6); // returns undefined without calling fn
+ */
