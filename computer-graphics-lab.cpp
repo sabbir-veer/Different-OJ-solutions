f253007942789f7a49b5baaba934b790c2368260
sabbir-veer <sabbir.hstu.19@gmail.com>
2023-10-29 01:22:24 +0600

    graphics.h

--- computer-graphics-lab.cpp
+++ computer-graphics-lab.cpp
@@ -2,7 +2,7 @@
 **ATTENTION**
 ? All tested solutions of computer graphics lab problems.
 ? Make sure your environment is set up appropriately otherwise you may face error running these codes.
-? file extension must be .cpp
+? file extension must be .cpp but do not use cpp streams such as cout, cin to avoid errors.
 ? here i'm using usleep(value) instead of delay() which is for Unix systems
 ? if you are using windows then replace it with Sleep(value)
 
@@ -12,8 +12,133 @@ gcc -o graphics graphics.cpp /usr/lib/libXbgi.a -lX11 -lm
 ./graphics
 
 last modified: 28 oct, 2023
-prepared by: Sabbir Ahmed
+prepared by: sabbir ahmed
 */
+
+/* Scaling*/
+#include <graphics.h>
+
+#include <iostream>
+void main() {
+    float p, q, r, s, Sx, Sy;
+
+    cout << "Enter the first coordinate of a line:";
+    cin >> p >> " " >> q;
+    cout << endl;
+
+    cout << "Enter the second coordinate of a line:";
+    cin >> r >> " " >> s;
+    cout << endl;
+
+    cout << "Enter the scaling factor:";
+    cin >> Sx >> " " >> Sy;
+    cout << endl;
+
+    int gd = DETECT, gm;
+    initgraph(&gd, &gm, (char *)"");
+    line(p, q, r, s);
+
+    p = p * Sx;
+    q = q * Sy;
+    r = r * Sx;
+    s = s * Sy;
+
+    line(p, q, r, s);
+
+    getch();
+    closegraph();
+}
+
+/* Ractangle Translation*/
+#include <bits/stdc++.h>
+#include <graphics.h>
+using namespace std;
+
+void translateRectangle(int P[][2], int T[]) {
+    int gd = DETECT, gm, errorcode;
+    initgraph(&gd, &gm, "c:\\tc\\bgi");
+    setcolor(2);
+    rectangle(P[0][0], P[0][1], P[1][0], P[1][1]);
+
+    P[0][0] = P[0][0] + T[0];
+    P[0][1] = P[0][1] + T[1];
+    P[1][0] = P[1][0] + T[0];
+    P[1][1] = P[1][1] + T[1];
+
+    rectangle(P[0][0], P[0][1], P[1][0], P[1][1]);
+    getch();
+    closegraph();
+}
+
+int main() {
+    int P[2][2] = {5, 8, 12, 18};
+    int T[] = {2, 1};  // translation factor
+    translateRectangle(P, T);
+    return 0;
+}
+
+/* Rotation of Triangle*/
+#include <graphics.h>
+#include <math.h>
+#include <stdio.h>
+
+void DrawTriangle(int x1, int y1, int x2, int y2, int x3, int y3);
+void RotateTriangle(int x1, int y1, int x2, int y2, int x3, int y3, float angle);
+
+int main() {
+    int gd = DETECT, gm;
+    int x1, y1, x2, y2, x3, y3;
+    float angle;
+
+    printf("Enter the 1st point for the triangle (x1 y1): ");
+    scanf("%d%d", &x1, &y1);
+
+    printf("Enter the 2nd point for the triangle (x2 y2): ");
+    scanf("%d%d", &x2, &y2);
+
+    printf("Enter the 3rd point for the triangle (x3 y3): ");
+    scanf("%d%d", &x3, &y3);
+
+    printf("Enter the angle for rotation (in degrees): ");
+    scanf("%f", &angle);
+
+    initgraph(&gd, &gm, (char *)"");
+    DrawTriangle(x1, y1, x2, y2, x3, y3);
+    RotateTriangle(x1, y1, x2, y2, x3, y3, angle);
+
+    getch();
+    closegraph();
+
+    return 0;
+}
+
+void DrawTriangle(int x1, int y1, int x2, int y2, int x3, int y3)
+
+{
+    line(x1, y1, x2, y2);
+    line(x2, y2, x3, y3);
+    line(x3, y3, x1, y1);
+}
+
+void RotateTriangle(int x1, int y1, int x2, int y2, int x3, int y3, float angle)
+
+{
+    int p = x2, q = y2;
+
+    float radianAngle = (angle * 3.14) / 180.0;
+
+    int a1 = p + (x1 - p) * cos(radianAngle) - (y1 - q) * sin(radianAngle);
+    int b1 = q + (x1 - p) * sin(radianAngle) + (y1 - q) * cos(radianAngle);
+
+    int a2 = p + (x2 - p) * cos(radianAngle) - (y2 - q) * sin(radianAngle);
+    int b2 = q + (x2 - p) * sin(radianAngle) + (y2 - q) * cos(radianAngle);
+
+    int a3 = p + (x3 - p) * cos(radianAngle) - (y3 - q) * sin(radianAngle);
+    int b3 = q + (x3 - p) * sin(radianAngle) + (y3 - q) * cos(radianAngle);
+
+    setcolor(1);
+    DrawTriangle(a1, b1, a2, b2, a3, b3);
+}
 
 /* DDA line drawing*/
 #include <graphics.h>
