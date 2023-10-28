9c747f229a131b93d1aafa0f176d1a15c85f9905
sabbir-veer <sabbir.hstu.19@gmail.com>
2023-10-29 01:33:59 +0600

    reflect

--- computer-graphics-lab.cpp
+++ computer-graphics-lab.cpp
@@ -140,6 +140,78 @@ void RotateTriangle(int x1, int y1, int x2, int y2, int x3, int y3, float angle)
     DrawTriangle(a1, b1, a2, b2, a3, b3);
 }
 
+/* Reflection of an object*/
+#include <graphics.h>
+#include <stdio.h>
+
+int main() {
+    // Initialize the drivers
+    int gm, gd = DETECT, ax, x1 = 100;
+    int x2 = 100, x3 = 200, y1 = 100;
+    int y2 = 200, y3 = 100;
+
+    initgraph(&gd, &gm, (char *)"");
+    // cleardevice();
+
+    // Draw the graph
+    line(getmaxx() / 2, 0, getmaxx() / 2,
+         getmaxy());
+    line(0, getmaxy() / 2, getmaxx(),
+         getmaxy() / 2);
+
+    // Object initially at 2nd quadrant
+    printf(
+        "Before Reflection Object"
+        " in 2nd Quadrant");
+
+    // Set the color
+    setcolor(14);
+    line(x1, y1, x2, y2);
+    line(x2, y2, x3, y3);
+    line(x3, y3, x1, y1);
+    getch();
+
+    // After reflection
+    printf("\nAfter Reflection");
+
+    // Reflection along origin i.e.,
+    // in 4th quadrant
+    setcolor(4);
+    line(getmaxx() - x1, getmaxy() - y1,
+         getmaxx() - x2, getmaxy() - y2);
+
+    line(getmaxx() - x2, getmaxy() - y2,
+         getmaxx() - x3, getmaxy() - y3);
+
+    line(getmaxx() - x3, getmaxy() - y3,
+         getmaxx() - x1, getmaxy() - y1);
+
+    // Reflection along x-axis i.e.,
+    // in 1st quadrant
+    setcolor(3);
+    line(getmaxx() - x1, y1,
+         getmaxx() - x2, y2);
+    line(getmaxx() - x2, y2,
+         getmaxx() - x3, y3);
+    line(getmaxx() - x3, y3,
+         getmaxx() - x1, y1);
+
+    // Reflection along y-axis i.e.,
+    // in 3rd quadrant
+    setcolor(2);
+    line(x1, getmaxy() - y1, x2,
+         getmaxy() - y2);
+    line(x2, getmaxy() - y2, x3,
+         getmaxy() - y3);
+    line(x3, getmaxy() - y3, x1,
+         getmaxy() - y1);
+    getch();
+
+    closegraph();
+
+    return 0;
+}
+
 /* DDA line drawing*/
 #include <graphics.h>
 #include <math.h>
